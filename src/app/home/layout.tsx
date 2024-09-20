c6296a74d77574783efdfd57ef0c632cf8f5c82a
kassianoshiguematsu <kassiano@weedvision.com.br>
2024-09-20 20:49:47 -0300

    first commit

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,15 @@
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
